5776b24fc6ef238ffb69294ff39db30992acf9cb
DefinitelyJuan <avilajuan0301@gmail.com>
2023-06-30 09:57:06 -0400

    spinnerkit

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -3,6 +3,7 @@ import 'package:geolocator/geolocator.dart';
 import 'location.dart';
 import 'location_screen.dart';
 import 'networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -17,6 +18,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   double? latitude;
   var locationWeather;
   void initState(){
+    getData();
     super.initState();
   }
 
@@ -27,19 +29,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
     latitude = location.latitude;
 
     locationWeather = await NetworkHelp("https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&units=metric&appid=ef7cc8cebcbb0e59326556672f3bdd2c").getData();
+    Navigator.push(context, MaterialPageRoute(builder: (context) => LocationScreen(locationWeather: locationWeather)));
 
   }
-
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async{
-            await getData();
-            Navigator.push(context, MaterialPageRoute(builder: (context) => LocationScreen(locationWeather: locationWeather)));
-          },
-          child: Text("Get Location"),
+        child:SpinKitDoubleBounce(
+         color: Colors.blue.shade900,
         ),
+        // child: ElevatedButton(
+        //   onPressed: () async{
+        //     await getData();
+        //     Navigator.push(context, MaterialPageRoute(builder: (context) => LocationScreen(locationWeather: locationWeather)));
+        //   },
+        //   child: Text("Get Location"),
+        // ),
       ),
     );
   }
